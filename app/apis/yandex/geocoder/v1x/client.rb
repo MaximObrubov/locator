eab0837e41430bbeeb63b9e13f678758262b1605
Maxim Obrubov <m.obrubov@yandex.ru>
2019-04-14 13:32:23 +0400

    fix get geodata from yandex geocoder by coordinates

--- app/apis/yandex/geocoder/v1x/client.rb
+++ app/apis/yandex/geocoder/v1x/client.rb
@@ -1,13 +1,38 @@
 module Yandex::Geocoder::V1x
 
   class Client
-    API_ENDPOINT = 'https://geocode-maps.yandex.ru/1.x'.freeze
+    API_ENDPOINT = 'https://geocode-maps.yandex.ru'.freeze
 
     def initialize()
+      raise "No geoapi token provided" unless ENV['YANDEX_GEOCODER_TOKEN']
       @token = ENV['YANDEX_GEOCODER_TOKEN']
+    rescue StandardError => err
+      puts "ERROR: #{err.message}"
+    end
 
-      puts @token
 
+    def point_info lat:, long:
+      request({
+        http_method: :get,
+        params: {geocode: "#{lat},#{long}"}
+      })
+    end
+
+
+    private
+
+    def adapter
+      @_adapter ||= Faraday.new(API_ENDPOINT) do |adapter|
+        adapter.adapter Faraday.default_adapter
+        adapter.ssl.verify = OpenSSL::SSL::VERIFY_PEER
+      end
+    end
+
+    def request http_method:, endpoint: '1.x/', params: {}
+      params[:apikey] = @token
+      params[:format] = 'json'
+      response = adapter.public_send(http_method, endpoint, params)
+      Oj.load(response.body)
     end
 
   end
--- lib/tasks/houses.rake
+++ lib/tasks/houses.rake
@@ -2,6 +2,9 @@ namespace :houses do
   desc "Adds new 50 houses to db"
   task add: :environment do
     geo = Yandex::Geocoder::V1x::Client.new()
+    data = geo.point_info(lat: 37.597576, long: 55.7718)
+
+    puts data.inspect
   end
 
 end
